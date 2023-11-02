131d29d217917a33a0c926cd80161e89ef02feef
rahul <rahuljangid4380@gmail.com>
2023-11-02 23:38:38 +0530

    first Commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import { BrowserRouter as Router, Route, Routes }
+from "react-router-dom";
+
+import Home from "./components/home/Home.jsx";
+import Footer from "./components/layout/Footer.jsx";
+import Header from "./components/layout/Header.jsx";
+import Contact from "./components/contact/Contact.jsx";
+import Cart from "./components/cart/Cart.jsx";
+import Shipping from "./components/cart/Shipping.jsx";
+import ConfirmOrder from "./components/cart/ConfirmOrder.jsx";
+import PaymentSuccess from "./components/cart/PaymentSuccess.jsx";
+import Login from "./components/login/Login";
+import Profile from "./components/profile/Profile";
+import MyOrders from "./components/myOrders/MyOrders";
+import OrderDetails from "./components/myOrders/OrderDetails.jsx";
+import Dashboard from "./components/admin/Dashboard.jsx";
+import Users from "./components/admin/Users.jsx";
+import Orders from "./components/admin/Orders.jsx";
+import About from "./components/about/About.jsx";
+import NotFound from "./components/layout/NotFound";
+
+import "./styles/app.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
+import "./styles/founder.scss";
+import "./styles/menu.scss";
+import "./styles/footer.scss";
+import "./styles/contact.scss";
+import "./styles/cart.scss";
+import "./styles/shipping.scss";
+import "./styles/confirmOrder.scss";
+import "./styles/paymentsuccess.scss";
+import "./styles/login.scss";
+import "./styles/profile.scss";
+import "./styles/table.scss";
+import "./styles/orderDetails.scss";
+import "./styles/dashboard.scss";
+import "./styles/about.scss";
+
+function App() {
+  return (
+    <Router>
+      <Header isAuthenticated={true} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/shipping" element={<Shipping />} />
+        <Route path="/confirmorder" element={<ConfirmOrder />} />
+        <Route path="/paymentsuccess" element={<PaymentSuccess />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/me" element={<Profile />} />
+        <Route path="/myorders" element={<MyOrders />} />
+        <Route path="/order/:id" element={<OrderDetails />} />
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/users" element={<Users />} />
+        <Route path="/admin/orders" element={<Orders />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/about/About.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { RiFindReplaceLine } from "react-icons/ri";
+import me from "../../assets/founder.jpg";
+
+const About = () => {
+    return (
+        <section className="about">
+            <main>
+                <h1>About Us</h1>
+
+                <article>
+                    <h4>Yummy Burgers</h4>
+                    <p>
+                        We are Yummy Burgers. The place for most tasty burgers on the
+                        enitre earth.
+                    </p>
+
+                    <p>
+                        Explore the various type of food and burgers. Click below to see the
+                        menu
+                    </p>
+
+                    <Link to="/">
+                        <RiFindReplaceLine />
+                    </Link>
+                </article>
+
+                <div>
+                    <h2>Founder</h2>
+                    <article>
+                        <div>
+                            <img src={me} alt="Founder" />
+                            <h3>Rahul Jangid</h3>
+                        </div>
+
+                        <p>
+                            I am Rahul Jangid, the founder of Yummy Burgers. Affiliated to
+                            God Taste...
+                        </p>
+                    </article>
+                </div>
+            </main>
+        </section>
+    );
+};
+
+export default About;
